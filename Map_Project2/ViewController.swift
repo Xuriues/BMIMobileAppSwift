2af7085f42a7c37b71fff8e830d833314ed8c54e
Shaun <shaunisawesome1139@gmail.com>
2020-08-07 19:15:05 +0800

    Initial Commit

---
+++ Map_Project2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Map_Project2
+//
+//  Created by Shaun on 7/8/20.
+//  Copyright © 2020 Shaun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
